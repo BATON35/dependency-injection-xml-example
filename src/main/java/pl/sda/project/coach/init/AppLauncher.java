53bf2a6af50436051e5388c0775f066c1d48978f
Konrad Bartczak <konradbartczak@onet.pl>
2018-06-03 00:44:59 +0200

    First bean in AppLauncher class.

--- src/main/java/pl/sda/project/coach/init/AppLauncher.java
+++ src/main/java/pl/sda/project/coach/init/AppLauncher.java
@@ -1,12 +1,17 @@
 package pl.sda.project.coach.init;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.sda.project.coach.beans.Coach;
-import pl.sda.project.coach.beans.TrackCoach;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        Coach theCoach = new TrackCoach();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("coachConfig.xml");
+
+       Coach theCoach = context.getBean("myCoach", Coach.class);
 
         System.out.println(theCoach.getDailyworkout());
+
+        context.close();
+
     }
 }
